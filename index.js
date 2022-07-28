264a143ed65cc4b9177a8f70272dc0372098abfe
ciko <ciko@mymail.com>
2022-07-28 13:54:43 +0200

    another change

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-console.log('Server is up on port ' + port)
-})
+
+import { createServer } from 'http';
+
+//create a server object:
+createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(port); //the server object listens on port 8080
 /*
 console.log("firts run");
 
