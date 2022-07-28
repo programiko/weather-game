a27fbcaa6f5ff2b26247efa272d72596162c7e07
ciko <ciko@mymail.com>
2022-07-28 13:42:04 +0200

    heroku

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+console.log('Server is up on port ' + port)
+})
+/*
 console.log("firts run");
 
 const fs = require('fs')
@@ -5,3 +10,4 @@ fs.writeFileSync('notes.txt', 'I live in Philadelphia and I gona...')
 
 const validator = require('validator')
 console.log(validator.isURL('https/mead.io')) // Print: true
+*/
